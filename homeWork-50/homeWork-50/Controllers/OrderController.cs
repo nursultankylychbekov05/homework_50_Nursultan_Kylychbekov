5e4f882bb786ae551b026dde305dafc46e0e14ba
Kylychbekov Nursultan <nursultankylychbekov05@gmail.com>
2026-08-18 18:08:36 +0600

    implement order creation ability and order listing

---
+++ homeWork-50/homeWork-50/Controllers/OrderController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using homeWork_50.Models;
+
+namespace homeWork_50.Controllers;
+
+public class OrderController : Controller
+{
+    private readonly StoreContext _context;
+
+    public OrderController(StoreContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IActionResult> Index()
+    {
+        var orders = await _context.Orders
+            .Include(o => o.Product)
+            .OrderByDescending(o => o.OrderDate)
+            .ToListAsync();
+
+        return View(orders);
+    }
+    
+    public async Task<IActionResult> Create(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product == null) return NotFound();
+
+        ViewBag.Product = product;
+        var order = new Order { ProductId = id };
+        return View(order);
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Order order)
+    {
+        var product = await _context.Products.FindAsync(order.ProductId);
+        if (product == null) return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            order.OrderDate = DateTime.Now;
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        ViewBag.Product = product;
+        return View(order);
+    }
+}
